7cff91888b78fa8d830a464674911b00e6d311b4
Daniel Mohr <daniel.mohr@uni-greifswald.de>
2022-11-07 15:23:06 +0100

    added a mock for Udp.h for running unittests

---
+++ src/udp_mock.h
@@ -0,0 +1,21 @@
+/*
+  Author: Daniel Mohr
+  Date: 2022-11-07
+
+  Since src/precise_sntp.cpp is compiled together with this unittest
+  test/unit_test_htonl_htons.cpp, we have to make at least the compiling
+  working.
+
+  For the unittest this file is used instead of Udp.h.
+*/
+
+class UDP {
+ public:
+  virtual uint8_t begin(uint16_t) = 0;
+  virtual int beginPacket(IPAddress ip, uint16_t port) = 0;
+  virtual size_t write(const uint8_t *buffer, size_t size) = 0;
+  virtual int endPacket() = 0;
+  virtual int parsePacket() = 0;
+  virtual int read(unsigned char* buffer, size_t len) = 0;
+  virtual int read(char* buffer, size_t len) = 0;
+};
