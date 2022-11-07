25efb9f8ce3bede5aa55ce3c9fc3a054905e5326
Daniel Mohr <daniel.mohr@uni-greifswald.de>
2022-11-07 14:39:40 +0100

    fixed test/unit_test_htonl_htons.cpp

--- test/unit_test_htonl_htons.cpp
+++ test/unit_test_htonl_htons.cpp
@@ -9,16 +9,16 @@
 #include <precise_sntp_htonl_htons.h>
 
 unittest(test_htons) {
-  uint16_t value = 0xab;
-  uint16_t result = 0xba;
+  uint16_t value = 0xABCD;
+  uint16_t result = 0xCDAB;
   assertEqual(htons(value), result);
   assertEqual(ntohs(htons(value)), value);
 }
 
 unittest(test_htonl) {
-  uint32_t value = 0xabcd;
-  uint32_t result = 0xdcba;
-  assertEqual(htonl(value), 0xdcba);
+  uint32_t value = 0x01234567UL;
+  uint32_t result = 0x67452301UL;
+  assertEqual(htonl(value), result);
   assertEqual(ntohl(htonl(value)), value);
 }
 
